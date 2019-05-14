5714a7cf74994008b30605c4a3399849e72df4db
grandalinho <grandalinho@gmail.com>
2019-05-14 15:34:13 -0300

    atulizar

---
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -0,0 +1,44 @@
+package com.prjcadcliente.persistencia;
+
+import java.util.List;
+
+import com.prjcadcliente.dominio.Cliente;
+
+/**
+ * <b>CRUDCLIENTE</B><br>
+ * Essa classe permite manipular as informações do cliente. aqui voce encontrara os seguintes comandos:
+ * <ul>
+ * <li>Cadastro</li>
+ * <li>Pesquisar por nome e por id</li>
+ * <li>Atualizar</li>
+ * <li>Deletar</li>
+ * </ul>
+ * 
+ * @author jonas.ncarvalho
+ *
+ */
+public class CRUDCliente {
+	
+	public String cadastrar(Cliente cliente) {
+		return null;
+	}
+	public String atualizar(Cliente cliente) {
+		return null;
+	}
+	
+	public String deletar(Cliente cliente) {
+		return null;
+	}
+	
+	public List<Cliente> PesquisarPorNome(String nome){
+		return null;
+	}
+	
+	public Cliente PesquisarPorId(int id){
+		return null;
+	}
+	
+	public Cliente PesquisarTodos() {
+		return null;
+	}
+}
